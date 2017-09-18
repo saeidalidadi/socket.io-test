144af1c03b0ecd9a43b5b43ca18966983a75cd7f
Saeid Alidadi <alidadisaeid@gmail.com>
2017-09-18 15:55:42 +0430

    feat: for socket behind proxy

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+const users = {};
+let lastID = 0;
+let disconnectTimeOut = 3000;
+
+io.on('connection', function(socket) {
+  let userId = socket.handshake.query.id;
+
+  if (userId === 'undefined') {
+    userId = lastID + 1;
+    lastID +=1;
+    socket.emit('yourID', userId);
+  }
+  users[userId] = {
+    lastConnection: Date.now(),
+    isConnected: true
+  };
+  console.log(users);
   console.log('Connected user id:', socket.id);
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
   socket.on('disconnect', function(data) {
     console.log('Disconnected:', socket.id);
-    io.emit('disconnect', socket.id);
-  })
+    users[userId].isConnected = false;
+    setTimeout(() => {
+    }, disconnectTimeOut);
+  });
+
 });
 
 http.listen(port, function(){
